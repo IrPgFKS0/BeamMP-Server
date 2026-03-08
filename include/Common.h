89392a67c26adbd447d1fee8947af3d0a2f978b4
Tixx <83774803+WiserTixx@users.noreply.github.com>
2026-03-08 18:16:10 +0100

    Bump version to 3.9.1

--- include/Common.h
+++ include/Common.h
@@ -129,7 +129,7 @@ private:
     static inline std::mutex mShutdownHandlersMutex {};
     static inline std::deque<TShutdownHandler> mShutdownHandlers {};
 
-    static inline Version mVersion { 3, 9, 0 };
+    static inline Version mVersion { 3, 9, 1 };
 };
 
 void SplitString(std::string const& str, const char delim, std::vector<std::string>& out);
