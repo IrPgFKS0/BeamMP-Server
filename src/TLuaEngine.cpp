0c864665bb52848ffb0525209944ecaf2edf19ad
Lion Kortlepel <development@kortlepel.com>
2026-04-16 18:46:48 +0200

    fix missing semicolon

--- src/TLuaEngine.cpp
+++ src/TLuaEngine.cpp
@@ -572,7 +572,7 @@ sol::table TLuaEngine::StateThreadData::Lua_GetPlayerIdentifiers(int ID) {
         }
         sol::table Result = mStateView.create_table();
         for (const auto& Pair : IDs) {
-            Result.set(Pair.first, Pair.second)
+            Result.set(Pair.first, Pair.second);
         }
         return Result;
     } else {
